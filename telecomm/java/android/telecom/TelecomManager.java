b478d4091f60f0b9f5ad571b9099fc1278aef86b
Thomas Stuart <tjstuart@google.com>
2022-06-23 14:27:43 -0700

    switch TelecomManager List getters to ParceledListSlice
    It was shown that given a large phoneAccountHandles that are
    over 1 mb, a TransactionTooLarge exception can be silently thrown
    causing an empty list to be returned.
    
    In order to prevent this behavior, all Lists that return a
    PhoneAccountHandle or PhoneAccount have been switched to
    ParceledListSlice.
    
    bug: 236263294
    Test: atest android.telecom.cts.PhoneAccountRegistrarTest
                 #testRegisterPhoneAccountHandleWithFieldOverLimit
    Change-Id: I025245b2a6f8cfaca86f268851a9d8f0817e07dd
    Merged-In: I025245b2a6f8cfaca86f268851a9d8f0817e07dd
    (cherry picked from commit 773cddde3d522606ff032fe8e432321c70edca09)
    Merged-In: I025245b2a6f8cfaca86f268851a9d8f0817e07dd

--- telecomm/java/android/telecom/TelecomManager.java
+++ telecomm/java/android/telecom/TelecomManager.java
@@ -685,7 +685,7 @@ public class TelecomManager {
         try {
             if (isServiceConnected()) {
                 return getTelecomService().getPhoneAccountsSupportingScheme(uriScheme,
-                        mContext.getOpPackageName());
+                        mContext.getOpPackageName()).getList();
             }
         } catch (RemoteException e) {
             Log.e(TAG, "Error calling ITelecomService#getPhoneAccountsSupportingScheme", e);
@@ -720,7 +720,7 @@ public class TelecomManager {
         try {
             if (isServiceConnected()) {
                 return getTelecomService().getCallCapablePhoneAccounts(
-                        includeDisabledAccounts, mContext.getOpPackageName());
+                        includeDisabledAccounts, mContext.getOpPackageName()).getList();
             }
         } catch (RemoteException e) {
             Log.e(TAG, "Error calling ITelecomService#getCallCapablePhoneAccounts(" +
@@ -739,7 +739,8 @@ public class TelecomManager {
     public List<PhoneAccountHandle> getPhoneAccountsForPackage() {
         try {
             if (isServiceConnected()) {
-                return getTelecomService().getPhoneAccountsForPackage(mContext.getPackageName());
+                return getTelecomService()
+                  .getPhoneAccountsForPackage(mContext.getPackageName()).getList();
             }
         } catch (RemoteException e) {
             Log.e(TAG, "Error calling ITelecomService#getPhoneAccountsForPackage", e);
@@ -793,7 +794,7 @@ public class TelecomManager {
     public List<PhoneAccount> getAllPhoneAccounts() {
         try {
             if (isServiceConnected()) {
-                return getTelecomService().getAllPhoneAccounts();
+                return getTelecomService().getAllPhoneAccounts().getList();
             }
         } catch (RemoteException e) {
             Log.e(TAG, "Error calling ITelecomService#getAllPhoneAccounts", e);
@@ -811,7 +812,7 @@ public class TelecomManager {
     public List<PhoneAccountHandle> getAllPhoneAccountHandles() {
         try {
             if (isServiceConnected()) {
-                return getTelecomService().getAllPhoneAccountHandles();
+                return getTelecomService().getAllPhoneAccountHandles().getList();
             }
         } catch (RemoteException e) {
             Log.e(TAG, "Error calling ITelecomService#getAllPhoneAccountHandles", e);
